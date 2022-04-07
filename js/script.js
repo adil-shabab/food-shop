96d67dfcfc32a9aca353969f74ee3c6367ecb05e
Adil Shabab <adilshabab2004@gmail.com>
2022-04-08 01:19:39 +0530

    typed script added

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
+
+
+var typed = new Typed('.animate', {
+    strings: [
+                "Your Favourite Dishes",
+                "In Your Door Step",
+                "Order Now From Our Shop",
+                "Enjoy Your Food"
+
+            ],
+            typeSpeed: 50,
+            backSpeed: 50,
+            loop: true
+});
+
+
+
 function add() {
     document.querySelector('.box-1').classList.toggle('.active')
 }
